d4c2beaadf48331620fa973aa81bdaeac2ef8940
Kasper Worm <kasper@sylvestworm.dk>
2017-10-02 21:17:36 +0200

    docs(notebook): fixed minor mistakes in notebook doc
    also added example under -h menu

--- notebook.sh
+++ notebook.sh
@@ -10,19 +10,22 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   echo "A good tip is to call this script once a day, so you always remember to"
   echo "update your diary"
   echo eg. making a cron task by adding this:
-  echo "*/30  7-17  *    *  1-5 DISPLAY=:0 xterm -e "bash ~/Scripts/diary.sh""
+  echo "*/30  7-17  *    *  1-5 DISPLAY=:0 xterm -e "bash ~/Scripts/notebook.sh""
   echo at the end of the file using crontab -e
   echo
   echo Arguments:
   echo -e '\t-h    :  show this help text'
-  echo -e '\targ1  :  directory location - default: ~Documents/Notes/'
-  echo -e '\targ2  :  filename           - default: note.txt'
+  echo -e '\targ2  :  directory location - default: ~Documents/Notes/'
+  echo -e '\targ1  :  filename           - default: note.txt'
+  echo  
+  echo -e '\teg:   :  ./notebook.sh note Notes'
+  echo -e '\t\t will make a new note.txt in ~/Notes'
   exit 0
 fi
 
 # Variables
-directory=~/${1:-Documents/Notes/}
-fileName=${2:-note}.txt
+directory=~/${2:-Documents/Notes/}
+fileName=${1:-note}.txt
 base=$directory/$fileName
 date=`date '+%Y-%m-%d %H:%M:%S'`
 
