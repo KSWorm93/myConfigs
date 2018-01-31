b8a75fb7365ff0ebc1be43640a3f2f134394eb21
Kasper Worm <kasper@sylvestworm.dk>
2018-01-31 20:45:26 +0100

    chore(moved): moved some stuff around, and added dotfiles for some configs

---
+++ dotfiles/openbox/autostart.sh
@@ -0,0 +1,26 @@
+#Panel
+tint2 &
+
+#Wallpaper
+nitrogen --restore &
+
+#App launcher
+albert &
+
+#Volume system tray
+(sleep 5; volumeicon) &
+
+#Compositor - eg transparancy
+(sleep 5; compton -b) &
+
+#Screen lock
+(sleep 10; light-locker) &
+
+#Filemanager daemon
+(sleep 5; thunar --daemon) &
+
+#Night-ligt
+(sleep 10; redshift) &
+
+#System monitoring
+(sleep 5; conky) &
